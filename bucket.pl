7cc43acc62b6b2f4a663a7d73c29ac93e02b48f7
Dan Boger <zigdon@irc.peeron.com>
2010-05-06 00:02:52 -0700

    Wrap the core of read_rss in an eval, just in case there are errors in the external sources

--- bucket.pl
+++ bucket.pl
@@ -3084,22 +3084,29 @@ sub read_rss {
     require XML::Simple;
 
     $LWP::Simple::ua->timeout(10);
-    my $rss = LWP::Simple::get($url);
-    if ($rss) {
-        Log "Retrieved RSS";
-        my $xml = XML::Simple::XMLin($rss);
-        for ( 1 .. 5 ) {
-            if ( $xml and my $story = $xml->{channel}{item}[ rand(40) ] ) {
-                $story->{description} =
-                  HTML::Entities::decode_entities( $story->{description} );
-                $story->{description} =~ s/$re//isg if $re;
-                next if $url =~ /twitter/ and $story->{description} =~ /^@/;
-                next if length $story->{description} > 400;
-                next if $story->{description} =~ /\[\.\.\.\]/;
+    eval {
+      my $rss = LWP::Simple::get($url);
+      if ($rss) {
+          Log "Retrieved RSS";
+          my $xml = XML::Simple::XMLin($rss);
+          for ( 1 .. 5 ) {
+              if ( $xml and my $story = $xml->{channel}{item}[ rand(40) ] ) {
+                  $story->{description} =
+                    HTML::Entities::decode_entities( $story->{description} );
+                  $story->{description} =~ s/$re//isg if $re;
+                  next if $url =~ /twitter/ and $story->{description} =~ /^@/;
+                  next if length $story->{description} > 400;
+                  next if $story->{description} =~ /\[\.\.\.\]/;
 
-                return ( $story->{description}, $story->{$tag} );
-            }
-        }
+                  return ( $story->{description}, $story->{$tag} );
+              }
+          }
+      }
+    }
+
+    if ($@) {
+      Report "Failed when trying to read RSS from $url: $@";
+      return ();
     }
 }
 
