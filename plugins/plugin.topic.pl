62feb803480477e36453281e68b2e3055de0668e
zigdon <zigdon@gmail.com>
2011-11-06 23:15:11 -0800

    move topic handling to a plugin. Allow plugins to register commands instead of implementing a full set of event handling

---
+++ plugins/plugin.topic.pl
@@ -0,0 +1,57 @@
+# BUCKET PLUGIN
+
+use BucketBase qw/say do Log yield/;
+
+my %topics;
+
+sub signals {
+  return (qw/jointopic on_topic/);
+}
+
+sub commands {
+  # label, addressed, operator, editable, re, callback
+  return ( { label => 'restore topic', addressed => 1, operator => 0, editable => 0,
+             re => qr/^restore topic$/i, callback => \&restore_nonop },
+           { label => 'restore topic', addressed => 1, operator => 1, editable => 0,
+             re => qr/^restore topic(?: (#\S+))/i, callback => \&restore_op },
+         );
+}
+
+sub route {
+  my ($package, $sig, $data) = @_;
+
+  &update_topic($data);
+
+  return 0;
+}
+
+sub update_topic {
+    my $data = shift;
+    Log "Topic in $data->{chl}: '$data->{topic}'";
+    $topics{$data->{chl}}{old} = $topics{$data->{chl}}{cur};
+    $topics{$data->{chl}}{cur} = $data->{topic};
+}
+
+sub restore_nonop {
+    my ($bag) = @_;
+
+    return &restore_topic($bag, $bag->{chl});
+}
+
+sub restore_op {
+    my ($bag) = @_;
+
+    return &restore_topic($bag, $1);
+}
+
+sub restore_topic {
+    my ($bag, $chl) = @_;
+
+    unless ( $topics{$chl} ) {
+        &say( $chl => "Sorry, $bag->{who}, I don't know what was the earlier topic!" );
+        return;
+    }
+    Log "$bag->{who} restored topic in $chl: $topics{$chl}{old}";
+    &say( $chl => "Okay, $bag->{who}." );
+    &yield( topic => $chl => $topics{$chl}{old} );
+}
