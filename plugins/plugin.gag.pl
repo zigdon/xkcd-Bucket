3b968070137bbbfff600a700e07cf0d57a7d9aec
Dan Boger <dan@peeron.com>
2012-04-17 16:31:22 -0700

    Gag should be applied per channel

--- plugins/plugin.gag.pl
+++ plugins/plugin.gag.pl
@@ -1,7 +1,7 @@
 # BUCKET PLUGIN
 
 use BucketBase qw/do config lookup talking Report/;
-my $gagged = 0;
+my %gagged;
 
 sub signals {
     return (qw/on_public say do/);
@@ -39,15 +39,15 @@ sub route {
     my ( $package, $sig, $data ) = @_;
 
     if ( $sig eq 'on_public' ) {
-        if (    $gagged
+        if (    $gagged{$data->{chl}}
             and &config("gagged_resist")
             and rand(100) < &config("gagged_resist") )
         {
             &lookup( chl => $data->{chl}, msg => &config("gagged_factoid") );
         }
 
-        if ( $gagged and $data->{addressed} and $data->{op} ) {
-            $gagged = 0;
+        if ( $gagged{$data->{chl}} and $data->{addressed} and $data->{op} ) {
+            $gagged{$data->{chl}} = 0;
             &talking( $data->{chl}, -1 );
             Report( "$data->{who} removed gag in $data->{chl} by addressing" );
         }
@@ -56,7 +56,7 @@ sub route {
     }
 
     # stop all processing if we're gagged.
-    if ( $gagged ) {
+    if ( $gagged{$data->{chl}} ) {
         return -1;
     }
 
@@ -70,13 +70,13 @@ sub shush {
     &do( $bag->{chl} => "is now gagged." );
     &talking( $bag->{chl}, 0 );
     Report( "$bag->{who} gagged in $bag->{chl}" );
-    $gagged = 1;
+    $gagged{$bag->{chl}} = 1;
 }
 
 sub free {
     my $bag = shift;
 
-    $gagged = 0;
+    $gagged{$bag->{chl}} = 0;
     &do( $bag->{chl} => "is FREE!" );
     &talking( $bag->{chl}, -1 );
     Report( "$bag->{who} removed gag in $bag->{chl}" );
